f6774ed981b3a209a007746dbca91b909ec63fd2
Danny Cho <dannycho7@gmail.com>
2019-04-07 20:52:20 -0700

    Stubs for shard

--- src/shard.py
+++ src/shard.py
@@ -1,9 +1,20 @@
 #! /usr/bin/env python3
 
+from lib.blockchain import Blockchain
+
 class Shard:
-	def __init__(self, i):
-		self.i = i
-	def getVote(self, vote_req):
+	def __init__(self, bch: Blockchain):
+		self.bch = bch
+		self.current_transaction = None
+		self.vote_decisions = []
+	def recvEndTransaction(self, mu):
+		"""
+		T_i, ts_i, r_w_set = mu (deconstruct)
+		self.current_transaction = mu
+		send signed(get_vote(b_i, mu)) to all shards in T_i
+		"""
+		pass
+	def recvGetVote(self, vote_req):
 		"""
 		if message contains valid signature then
 			decide to commit or abort T_i
@@ -11,5 +22,35 @@ class Shard:
 			if decision = commit then
 				construct VO_i for the tuples accessed in T_i
 			send(signed(vote(decision, VO_i, root_{i-1}))) to vote requester (Coordinator)
+		"""
+		pass
+	def recvVote(self, vote):
+		"""
+		add to voted store
+		check if all involved shards voted
+			final_decision <- commit
+			corrupted_shards <- {}
+			for each shard S_j do
+				fetch the last known root last_roots[S_j]
+				if root_{i-1} =/= last_roots[S_j] then
+					final_decision <- abort
+					append S_j to corrupted shards
+				else if decision = abort then
+					final_decision <- abort
+			if final_decision = commit then
+				update last_roots[S_j] for each shard S_j
+			send signed(decision(T_i, final_decision)) to all shards
+		"""
+		pass
+	def recvPrepare(self, decision, ch, b_i):
+		"""
+		check if decision is abort
+		send Ack to coordinator with response
+		"""
+		pass
+	def recvAck(self, res):
+		"""
+		aggregate all schnorr-responses to form collective signature
+		send b_i and multisig to client and cohorts.
 		"""
 		pass

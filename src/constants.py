997047b48d1f63193685db1ff47fc11cf838b394
sujaya <sujayamaiya@gmail.com>
2019-05-06 14:55:20 -0700

    Cleaning up constants file

--- src/constants.py
+++ src/constants.py
@@ -1,15 +1,9 @@
 
 class Const(object):
-    """docstring for Constants"""
-    # def __init__(self, arg):
-    #     super(ClassName, self).__init__()
-    #     self.arg = arg
         
     NUM_ELEMENTS = 1000
     NUM_PARTITIONS = 2
-    NULL = 0
 
     #Client side constants 
-    CONCURRENCY = 20
-    NUM_TXNS = 1
+    NUM_TXNS = 10
     NUM_OPS = 5

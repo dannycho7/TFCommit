e3514a37ae5c0941dc6409381cfbf8c36e3782f1
sujaya <sujayamaiya@gmail.com>
2019-05-16 17:56:46 -0700

    Bug fix

--- src/client.py
+++ src/client.py
@@ -100,7 +100,7 @@ if __name__ == "__main__":
     config = json.load(open(sys.argv[1]))
     cl = Client(config)
     verbose = bool(int(sys.argv[2]))
-    num_elements = config['client']['num_elements']
+    num_elements = config['num_elements']
     num_ops = int(sys.argv[3])
     client_config = config['client']
     client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
